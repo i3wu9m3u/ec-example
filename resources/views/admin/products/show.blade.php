3178c6c918096c944f7c66adb0880e56e09bbcfc
i3wu9m3u <63505092+i3wu9m3u@users.noreply.github.com>
2020-04-25 11:11:50 +0900

    Enable to confirm the datetime of product registration.

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -31,5 +31,11 @@
                 {{ $product->price }}
             </div>
         </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>CreatedAt:</strong>
+                {{ $product->created_at }}
+            </div>
+        </div>
     </div>
 @endsection
